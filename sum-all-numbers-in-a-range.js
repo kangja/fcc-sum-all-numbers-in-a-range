1414a2bd3ed1eccceabe65a099e38dfe284506a8
hi <kangja1996@gmail.com>
2020-10-07 09:57:10 -0400

    progress

---
+++ sum-all-numbers-in-a-range.js
@@ -0,0 +1,36 @@
+// *******************************************
+
+// Intermediate Algorithm Scripting: Sum All Numbers in a Range
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+
+// For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+
+
+// sumAll([1, 4]) should return a number.
+// sumAll([1, 4]) should return 10.
+// sumAll([4, 1]) should return 10.
+// sumAll([5, 10]) should return 45.
+// sumAll([10, 5]) should return 45.
+
+// *******************************************
+
+
+// *****************************************PSEUDOCODE***************************************
+
+// 1+2+3+4 = 10
+// let sum = 0;
+// WHEN GIVEN two numbers, FIRST ORDER IT FROM LOWEST TO HIGHEST 
+
+// if x < y {
+//   do for loop from x to y
+// }
+
+// if y > x {
+//   do for loop from x to y
+// }
+
+//
+
+// 3.
+
+// // **************************************************************************************
